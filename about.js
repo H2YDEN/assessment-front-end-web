93de3aaedcbd1340ecbf7da2251cb129656b5675
H2YDEN <h2yden2@gmail.com>
2024-02-20 16:39:26 -0800

    Assessment completed

--- about.js
+++ about.js
@@ -8,10 +8,8 @@ function handleSubmit(evt) {
 	alert("Form successfully submitted!");
 }
 
-// Get all elements with the class 'image'
 const duck = document.querySelector('img[src="https://basilwoodfarm.com/cdn/shop/products/istockphoto-185590965-612x612_1024x1024.jpg?v=1648592027"]');
 
-// Function to display a compliment
 function displayCompliment() {
     alert("You're looking great today!");
 }
